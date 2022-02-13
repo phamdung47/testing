8abeddd5c8d497ebf60c994c49f43baa6b65770b
phamdung47 <64241672+phamdung47@users.noreply.github.com>
2022-02-13 09:35:59 -0500

    Update divide2int.cpp

--- divide2int.cpp
+++ divide2int.cpp
@@ -35,11 +35,48 @@ Constraints:
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        return 0;
+        if (divisor == 1)
+            return dividend;
+        if (divisor == -1)
+            return neg(dividend);
+        if (divisor == 0)
+            return 2147483647;
+        int sign=1, quotient=0, extra=0;
+        if (dividend < 0) {
+            sign = -sign;
+            dividend = neg(dividend);
+        }
+        if (divisor < 0) {
+            sign = -sign;
+            divisor = neg(divisor);
+        }
+        while (dividend >= divisor) {
+            int shift = 0, temp;
+            while ((temp = (divisor << shift)) != -2147483648 && temp <= dividend)
+                shift += 1;
+            quotient += (1 << (shift - 1));
+            dividend -= divisor << (shift - 1);
+        }
+        return (sign == -1 ? neg(quotient) : quotient);
+    }
+	
+private:
+    int neg(int num) {
+        if (num == 0)
+            return num;
+        if (num < 0) {
+            if (num == -2147483648)
+                return 2147483647;
+        }
+        else {
+            if (num == 2147483647)
+                return -2147483648;
+        }
+        return -num;
     }
 };
 
-#if 0
+#if 1
 //----------------------------------------------------------------------------
 // Main
 //----------------------------------------------------------------------------
@@ -49,15 +86,26 @@ int main() {
         int divisor;
         int answer;
     } in[] = {
+        {-2147483648, 2, -1073741824},
+        {2, 2, 1},
+        {1, -1, -1},
         {10, 3, 3},
         {7, -3, -2},
+        {0, 1, 0},
+        {1, 1, 1},
+        {253, 7, 36},
+        {2147483647, 1, 2147483647},
+        {-2147483647, 2, -1073741823},
+        {2147483647, 2, 1073741823},
+        {-2147483648, 1, -2147483648},
+        {-2147483648, -1, 2147483647},
     };
     Solution sol;
     int cnt = sizeof(in)/sizeof(in[0]);
     
     for (int i=0; i < cnt; i++) {
         int quotient = sol.divide(in[i].dividend, in[i].divisor);
-        printf("%d / %d = %d %s\n", in[i].dividend, in[i].divisor,
+        printf("%d / %d = %d  <%s>\n", in[i].dividend, in[i].divisor,
                quotient, (quotient == in[i].answer ? "correct" : "wrong"));
     }
     return 0;
